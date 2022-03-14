15ed70fefe9b5c726845d9028a093fb5fbaf387c
gureumwoon <vfrvfr3221@gmail.com>
2022-03-14 23:57:59 +0900

    Banner styled-component

--- react-netflix-clone/src/App.js
+++ react-netflix-clone/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './components/Nav';
 import Banner from './components/Banner';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Banner />
+      <Footer />
     </div>
   );
 }
