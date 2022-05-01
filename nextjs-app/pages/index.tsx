c650b05e5dd2c8c0f5f4122af88f8454b6eff550
gureumwoon <vfrvfr3221@gmail.com>
2022-05-01 23:58:39 +0900

    getStaticProps를 이용한 포스트 리스트 나열

--- nextjs-app/pages/index.tsx
+++ nextjs-app/pages/index.tsx
@@ -24,7 +24,15 @@ const Home = ({ allPostsData }: {
       <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
         <h2 className={homeStyles.headingLg}>Blog</h2>
         <ul className={homeStyles.list}>
-
+          {allPostsData.map(({ id, title, date }) =>
+            <li className={homeStyles.listItem} key={id}>
+              <a>{title}</a>
+              <br />
+              <small className={homeStyles.lightText}>
+                {date}
+              </small>
+            </li>
+          )}
         </ul>
       </section>
     </div>
